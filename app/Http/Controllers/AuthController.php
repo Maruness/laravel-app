ac9c7b1ec592a041090c1b5cc099363fa89de245
Maruness <chrismarbelostrino@gmail.com>
2025-04-24 19:00:18 +0800

    added register function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\rquest;
-use App\Model\User;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 
-abstract class Controller
+class AuthController extends \Illuminate\Routing\Controller
 {
   public function index() {
-
+    
   }
   
   public function login() {
 
   }
 
-  public function register() {
+  public function register(Request $request) {
+    try {
+      $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|string|min:8',
+      ]);
 
+      User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+      ]);
+
+      return redirect('/')->with('success', 'User successfully registered.');
+    } catch(\Exception $e) {
+      echo $e;
+    }
   }
 
   public function logout() {
