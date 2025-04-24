d0039f69d50621ca7e30527dbe4f111413b9d284
Maruness <chrismarbelostrino@gmail.com>
2025-04-24 19:17:54 +0800

    added a comment

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -8,7 +8,7 @@
   <div class="max-w-2xl mx-auto">
     <div class="bg-gray-900 overflow-hidden shadow-sm sm:rounded-lg">
       <div class="p-6 border-b border-gray-200">
-        {{$slot}}
+        {{$slot}} {{-- to be substituted by other blade.php files --}}
       </div>
     </div>
   </div>
