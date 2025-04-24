00962ea67275fbab569d5fe9da99367580cf5f76
Maruness <chrismarbelostrino@gmail.com>
2025-04-24 21:55:27 +0800

    added middleware control for unauthenticated users

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('database');
-});
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
+
+Route::get('/login', function () {/* display login page */
+  return view('login');
+})->name('login');/* needs ->name to make the middleware find the routing */
+Route::get('/register', function () {/* display register page */
+  return view('register');
 });
 
-Route::prefix('authentication')->group(function () {
-    Route::post('/user', [AuthController::class, 'index']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');/* executes logout function from authcontroller */
+
+Route::prefix('authentication')->group(function () {/* auth pages for method POST */
+  Route::post('/user', [AuthController::class, 'index']);
+  Route::post('/login', [AuthController::class, 'login']);
+  Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::middleware(['auth'])->group(function () {/* redirects unauthenticated users to login */
+  Route::get('/', function () {
+    return view('home');
+  });
 });
