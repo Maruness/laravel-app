1cbb80f08c61d9809b39a7371b1211471e60a8e8
Maruness <chrismarbelostrino@gmail.com>
2025-04-24 18:15:20 +0800

    added routes for authentications

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('database');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::prefix('authentication')->group(function () {
+    Route::post('/user', [AuthController::class, 'index']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
