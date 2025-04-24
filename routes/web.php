9528c8f334f6a37b0997b626f449a06af0eb7c65
Maruness <chrismarbelostrino@gmail.com>
2025-04-25 03:42:54 +0800

    reorganize revert

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/login', function () {/* display login page */
