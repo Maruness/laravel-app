f65486c5262383fbf4d88c9858869b0f01333df3
Maruness <chrismarbelostrino@gmail.com>
2025-04-24 17:00:00 +0800

    added placeholders for auth functions

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\rquest;
+use App\Model\User;
+
+abstract class Controller
+{
+  public function index() {
+
+  }
+  
+  public function login() {
+
+  }
+
+  public function register() {
+
+  }
+
+  public function logout() {
+
+  }
+}
