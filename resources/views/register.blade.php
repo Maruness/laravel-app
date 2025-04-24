875455f7bc2ca6865494cb317faf718408ed9540
Maruness <chrismarbelostrino@gmail.com>
2025-04-24 19:08:44 +0800

    added register form and comments

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,4 +1,19 @@
-<!DOCTYPE html>
-<html>
-
-</html>
+<x-base>{{-- takes base.blade.php as a layout --}}
+<title>Register</title>
+<form method="POST" action="/authentication/register" class="max-w-sm mx-auto">
+  @csrf
+  <div class="mb-5">{{-- name textbox --}}
+    <label for="name" class="form-label">Name</label>
+    <input type="text" id="name" name="name" class="form-box" placeholder="name" required />
+  </div>
+  <div class="mb-5">{{-- email textbox --}}
+    <label for="email" class="form-label">Email</label>
+    <input type="email" id="email" name="email" class="form-box" placeholder="email@provider.com" required />
+  </div>
+  <div class="mb-5">{{-- password textbox --}}
+    <label for="password" class="form-label">Password</label>
+    <input type="password" id="password" name="password" class="form-box" required />
+  </div>
+  <button type="submit" class="btn-submit">Submit</button>
+</form>
+</x-base>
